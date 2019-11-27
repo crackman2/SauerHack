3b21521a083bb57f24c4c4fe1f889a5e7c8cfb10
Jackson Smackson <crackman@instantmail.fr>
2019-11-27 12:45:14 +0100

    Project started

---
+++ unit1.pas
@@ -0,0 +1,31 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Windows;
+
+type
+  TForm1 = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  posx: Single;
+  posy: Single;
+  posz: Single;
+  camx: Single;
+  camy: Single;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
