84f904b7d174b4649c19afdabd29092d62af200f
Jackson Smackson <pombenenge@googlemail.com>
2019-11-29 03:27:18 +0100

    Added OpenGL SwapBuffers thingy for future use

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ExtCtrls,  jwapsapi,MouseAndKeyInput,LCLType,
+  ExtCtrls, jwapsapi, MouseAndKeyInput, LCLType,
   Windows;
 
 type
@@ -15,10 +15,12 @@ type
 
   TForm1 = class(TForm)
     btnInit: TButton;
+    btnOpenGL: TButton;
     LogBox: TListBox;
     Triggerbot: TTimer;
     TimerFly: TTimer;
     procedure btnInitClick(Sender: TObject);
+    procedure btnOpenGLClick(Sender: TObject);
     procedure CheckBoxFlyChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure LogBoxClick(Sender: TObject);
@@ -43,7 +45,7 @@ var
   addposz: DWORD;
   addcamx: DWORD;
   addcamy: DWORD;
-  addwepdelay:DWORD;
+  addwepdelay: DWORD;
 
   Velocity: single = 3.0;
   BoostVelocity: single = 0;
@@ -53,7 +55,7 @@ var
 
   MouseX: integer = 0;
   MouseY: integer = 0;
-  TriggerEnabled:Boolean = False;
+  TriggerEnabled: boolean = False;
 
   hProcess: HANDLE = 0;
   ProcId: DWORD = 0;
@@ -83,6 +85,11 @@ begin
 end;
 
 procedure WriteByte(Address: DWORD; Value: byte);
+begin
+  WriteProcessMemory(hProcess, Pointer(Address), @Value, sizeof(Value), nil);
+end;
+
+procedure WriteDWORD(Address: DWORD; Value: DWORD);
 begin
   WriteProcessMemory(hProcess, Pointer(Address), @Value, sizeof(Value), nil);
 end;
@@ -234,20 +241,21 @@ begin
 end;
 
 procedure TForm1.TriggerbotTimer(Sender: TObject);
-var foo:Integer=0;
-    Delay:Integer=0;
+var
+  foo: integer = 0;
+  Delay: integer = 0;
 begin
-   if (GetAsyncKeyState(VK_X) <> 0) then
+  if (GetAsyncKeyState(VK_X) <> 0) then
   begin
     if TriggerEnabled then
     begin
-          Log('Triggerbot disabled!');
-          TriggerEnabled:=false;
+      Log('Triggerbot disabled!');
+      TriggerEnabled := False;
     end
     else
     begin
-         Log('Triggerbot enabled!');
-         TriggerEnabled:=true;
+      Log('Triggerbot enabled!');
+      TriggerEnabled := True;
     end;
 
 
@@ -269,7 +277,8 @@ begin
       Sleep(200);
     end;
   end;
-  ReadProcessMemory(hProcess,Pointer(addwepdelay),@Delay,sizeof(delay),0);//Check weapon delay
+  ReadProcessMemory(hProcess, Pointer(addwepdelay), @Delay, sizeof(delay), nil);
+  //Check weapon delay
 
   {
   if (GetAsyncKeyState(VK_L) <> 0)then
@@ -282,24 +291,24 @@ begin
   end;
   }
 
-  if TriggerEnabled  and (Delay = 0) then
+  if TriggerEnabled and (Delay = 0) then
+  begin
+    foo := GetPixel(GetDC(0), MouseX, MouseY);
+    //Log(IntToHex(foo,6));
+    if (foo = $02FF00) then
     begin
-       foo:=GetPixel(GetDC(0),MouseX,MouseY);
-       //Log(IntToHex(foo,6));
-       if(foo = $02FF00)then
-         begin
-               //MouseInput.Click(mbLeft,[]);
-               //SendMessage(hwnd_outra_win,WM_LBUTTONDOWN,MK_LBUTTON,MAKELPARAM(pos_cursor.x,pos_cursor.y));
-               //SendMessage(hwnd_outra_win,WM_LBUTTONUP,MK_LBUTTON,MAKELPARAM(pos_cursor.x,pos_cursor.y));
+      //MouseInput.Click(mbLeft,[]);
+      //SendMessage(hwnd_outra_win,WM_LBUTTONDOWN,MK_LBUTTON,MAKELPARAM(pos_cursor.x,pos_cursor.y));
+      //SendMessage(hwnd_outra_win,WM_LBUTTONUP,MK_LBUTTON,MAKELPARAM(pos_cursor.x,pos_cursor.y));
 
-               SendMessage(hWindow,WM_LBUTTONDOWN,MK_LBUTTON,MAKELPARAM(MouseX,MouseY));
-               Sleep(10);
-               SendMessage(hWindow,WM_LBUTTONUP,MK_LBUTTON,MAKELPARAM(MouseX,MouseY));
+      SendMessage(hWindow, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(MouseX, MouseY));
+      Sleep(10);
+      SendMessage(hWindow, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(MouseX, MouseY));
 
 
-                Log('Click!');
-         end;
+      Log('Click!');
     end;
+  end;
 end;
 
 function GetModuleBaseAddress(ProcessID: cardinal; MName: string): Pointer;
@@ -365,7 +374,7 @@ begin
     addposz := Base + $38;
     addcamx := Base + $3C;
     addcamy := Base + $40;
-    addwepdelay:= Base + $174;
+    addwepdelay := Base + $174;
 
     Log('Addposx: 0x' + IntToHex(addposx, 6));
     Log('Addposy: 0x' + IntToHex(addposy, 6));
@@ -380,6 +389,88 @@ begin
     Log('Sauerbraten was not found!');
     Log('May require admin privileges');
   end;
+
+end;
+
+
+
+procedure TForm1.btnOpenGLClick(Sender: TObject);
+var
+  OpenGlBase: DWORD = 0; //Found with GetModuleBase
+  SwapBuffersOffset: DWORD = $45E21; //OPENGL32.dll+45e21
+  SwapBuffers: DWORD = 0;
+  JumpToLocation: DWORD = $E90016; //Common code cave (Actually at 0x10300)
+  JumpBackFromLocation:DWORD = $E90016+9;
+  JumpBytes:DWORD = 0;
+  JumpBackBytes:DWORD=0;
+begin
+  //00
+  Log('OpenGL stuff executing..');
+  OpenGlBase := DWORD(GetModuleBaseAddress(ProcId, 'opengl32.dll'));
+  Log('OpenGL.dll Base: 0x' + IntToHex(OpenGlBase, 6));
+  SwapBuffers := OpenGlBase + SwapBuffersOffset;
+  Log('SwapBuffers at 0x' + IntToHex(SwapBuffers, 6));
+  // JMP to 10300 -> 5 Bytes long
+  // Original code at SwapBuffers is
+  // mov edi,edi (does nothing :/)                 8B FF
+  // push ebp                                      55
+  // mov ebp,esp                                   8B EC
+
+  //JUMP at SwapBuffers to Your Code's address (eg. 100010300 - 5(Subtract 5 bytes
+  //to accomodate the jump)) - Swapbuffers (74BD5E21)
+  //=> 8B43A4DA. Then convert edianess (SwapEdian(?)) to DAA4438B. Add E9 as
+  //first byte to be written followed by DF A4 43 8B
+  //Result: E9 DAA4438B (JMP 10300)
+
+  //at 10300 do the following
+  //pushfd
+  //pushad
+  //-> YOUR CODE HERE
+  //popad
+  //popfd
+  //-> ORIGINAL CODE
+  //JMP back to below the original jump at SwapBuffers (Probably
+  //WRITING THE JUMP SHOULD BE DONE LAST!
+  Log('Calculating jump offset...');
+  JumpBytes:= (($100000000 + JumpToLocation)-5) - SwapBuffers; // Calculating jump offset
+  Log('JumpBytes 0x' + IntToHex(JumpBytes, 6));
+  //JumpBytes:=SwapEndian(JumpBytes); //swapping edian  // turns out this is not needed//writeprocessmemory is smart enough
+  Log('JumpBytes (Edian swapped) 0x' + IntToHex(JumpBytes, 6));
+  Log('Writing Jump...');
+  WriteByte(SwapBuffers,$E9);
+  WriteDWORD(SwapBuffers+1,JumpBytes);
+  Log('Done.');
+
+  //The Jump back to Swapbuffer is
+  //the address where the jump back is written (eg. 10309) +
+  //(Swapbuffers+5(right after the original jump) - 5 (size of jump (since we
+  //don't loop around this time the +5 and -5 sizes of the jumps cancel out)
+  //
+  //Location right after Swapbuffers(Swapbuffers+5)-5(size of jump (since we
+  //don't loop around this time the +5 and -5 sizes of the jumps cancel out) -
+  //address where the jump back is written (eg. 10309)
+  Log('Calculating jump back offset...');
+  JumpBackBytes:=(Swapbuffers+5)-5-JumpBackFromLocation;
+  Log('JumpBackBytes 0x' + IntToHex(JumpBackBytes, 6));
+  Log('Writing into CodeCave (Example code)');
+  WriteByte($10300,$9C); //push fd
+  WriteByte($10301,$60); //push ad
+  WriteByte($10302,$61); //pop  ad
+  WriteByte($10303,$9D); //pop  fd
+  WriteByte($10304,$8B); WriteByte($10305,$FF); //mov edi,edi
+  WriteByte($10306,$55); //push ebp
+  WriteByte($10307,$8B); WriteByte($10308,$EC); //mov ebp,esp
+  Log('Done.');
+  Log('Writing JumpBackBytes...');
+  WriteByte(JumpBackFromLocation,$E9);
+  WriteDWORD(JumpBackFromLocation+1,JumpBackBytes);
+  log('Done.');
+  log('OpenGL stuff completed');
+
+
+
+
+
 
 end;
 
